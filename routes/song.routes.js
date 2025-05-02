7298feb5bf22a474a4491e730406cfdab9186192
DN242005 <paz76242@gamil.com>
2025-05-02 16:43:33 -0600

    🐛 Fix: Corrige el nombre del controlador en rutas

--- routes/song.routes.js
+++ routes/song.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/song.controller");
+const controller = require("../controllers/songController");
 
 // Crear canción
 router.post("/songs", controller.createSong);
